c93b6b0fef7751b883e8c94dbd2fbe625f59da6d
Vu Pham <vupham326@gmail.com>
2019-01-23 01:12:32 -0800

    screen saver project

---
+++ MyScreenSaver/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace MyScreenSaver
+{
+     partial class frmScSaver
+     {
+          /// <summary>
+          /// Required designer variable.
+          /// </summary>
+          private System.ComponentModel.IContainer components = null;
+
+          /// <summary>
+          /// Clean up any resources being used.
+          /// </summary>
+          /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+          protected override void Dispose(bool disposing)
+          {
+               if (disposing && (components != null))
+               {
+                    components.Dispose();
+               }
+               base.Dispose(disposing);
+          }
+
+          #region Windows Form Designer generated code
+
+          /// <summary>
+          /// Required method for Designer support - do not modify
+          /// the contents of this method with the code editor.
+          /// </summary>
+          private void InitializeComponent()
+          {
+               this.components = new System.ComponentModel.Container();
+               this.timer1 = new System.Windows.Forms.Timer(this.components);
+               this.SuspendLayout();
+               // 
+               // timer1
+               // 
+               this.timer1.Enabled = true;
+               this.timer1.Interval = 10000;
+               this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+               // 
+               // frmScSaver
+               // 
+               this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+               this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+               this.BackColor = System.Drawing.Color.Black;
+               this.ClientSize = new System.Drawing.Size(926, 835);
+               this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+               this.Name = "frmScSaver";
+               this.ShowInTaskbar = false;
+               this.Text = "My Screen Saver";
+               this.TopMost = true;
+               this.UseWaitCursor = true;
+               this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+               this.Load += new System.EventHandler(this.frmScSaver_Load);
+               this.Paint += new System.Windows.Forms.PaintEventHandler(this.frmScSaver_Paint);
+               this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmScSaver_KeyDown);
+               this.ResumeLayout(false);
+
+          }
+
+          #endregion
+
+          private System.Windows.Forms.Timer timer1;
+     }
+}
+
